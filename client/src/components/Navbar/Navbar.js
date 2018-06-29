bdaa74cced5e7642ad5ab99c303fb1ba672581ca
Blonded <Lena@lenas-mbp.attlocal.net>
2018-06-28 23:06:30 -0700

    added navbar and file structures for pages and components

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = props => (
+
+<nav className="navbar sticky-top navbar-expand-lg">
+
+  <a className="navbar-brand" href="/">Fashi.On</a>
+  <a className="navbar-brand" href="/login">Log-in</a>
+  <a className="navbar-brand" href="/signup">Sign up!</a>
+
+</nav>
+
+
+
+)
+
+
+export default Navbar;
---
+++ client/src/pages/Signup/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Signup.css";
+
+
+
+const Signup = () => (
+
+
+);
+
+
+export default Signup;
