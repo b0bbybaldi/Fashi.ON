0acbe5fce7ce41c502eb40ff13e1ffb2a4db425d
Mridula Udaya Shankar <mridu.sh92@gmail.com>
2018-06-29 15:54:20 -0700

    Create routes for User model , Add Connection to MongoDB and Add Controllers

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
+const routes = require("./routes");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,14 +13,13 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+//connection to mongo DB 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fashiondb");
+
 
 // Define API routes here
-
+app.use(routes);
 // Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
