a4a938f02c983e9ed7e426f0196dd9ff9e663328
Blonded <Lena@lenas-mbp.attlocal.net>
2018-07-03 00:21:47 -0700

    settings page

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,12 +1,33 @@
 import React from "react";
 import "./Signup.css";
 import Header from '../../components/Header/Header.js';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
 
 
 const Signup = () => (
   <div>
   <Header />
-  <h1> Sign up!!</h1>
+<div>
+      <Paper className= "" elevation={1}>
+        <Typography variant="headline" component="h3">
+          Were happy to have you here.
+        </Typography>
+        <Typography component="p">
+          Please fill out the form below in order to continue to your personalized
+          profile.
+        </Typography>
+<p> what is your name?? </p>
+
+
+
+
+
+      </Paper>
+    </div>
+
+
   </div>
 
 );
