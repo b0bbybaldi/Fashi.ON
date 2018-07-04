2c82cf02197af31e2a2c58608b05d67b229ee979
saranasr83 <saranasr83@gmail.comgit config --global user.email saranasr83@gmail.comgit config --global user.email saranasr83@gmail.com>
2018-07-03 17:04:20 -0700

    initialize the passport

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,22 @@
 const db = require('../models');
 
+
 module.exports = {
-    findById: function(req, res){
+    findById:  passport.authenticate("local", function(req, res){
         db.User
         .findById(req.params.id)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
-    },
-    create: function(req,res){
+    }),
+    create: passport.authenticate ("local", function(req,res) {
         db.User
-        .create(req.body)
+        .create({
+            email: req.body.email,
+            password: req.body.password
+        })
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
-    },
+    }),
     update: function(req,res){
         db.User
         .findOneAndUpdate({ _id: req.params.id } ,req.body)
@@ -20,3 +24,11 @@ module.exports = {
         .catch(err => res.status(422).json(err));
     }
 };
+
+// if (req.user){
+//     req.session.destroy(function (err) {
+//         req.logout();
+//         res.redirect("/");
+//     })
+// } else
+// res.redirect("/login")
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,12 +1,32 @@
 const router = require("express").Router();
 const userController = require('../../controllers/userController');
 
-//login route
-router.route('/')
-    .get(userController.findById);
+//require passport
+var passport = require("../passport/passport.js");
+var path = require("path");
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../passport/isAuthenticated.js");
 
-router.route('/api/user')
+//signup route
+
+router.route('/api/newuser')
     .post(userController.create);
+
+//login route
+router.route('/login')
+.get(userController.findById);
+
+//logout route
+// router.route("/logout")
+// if (req.user){
+//     req.session.destroy(function (err) {
+//         res.clearCookie('connect.sid')
+//         req.logout();
+//         res.redirect("/");
+//     })
+// } else
+// res.redirect("/login")
+
 
 //login specific to the user by id for profile page
 router.route('/api/user/:id')
