c645584237af83e1c487f456d766707af4655168
Gustavo Gibo <gustavo.gibo@gmail.com>
2018-07-05 16:10:21 -0700

    retrieving some stuff back from login and signup

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./Signup.css";
-import API from "../../utils/API";
 import Header from '../../components/Header/Header.js';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
@@ -8,13 +7,13 @@ import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
-// import FormControl from '@material-ui/core/FormControl';
+import FormControl from '@material-ui/core/FormControl';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import IconButton from '@material-ui/core/IconButton';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
-// import classNames from 'classnames';
-import Button from '../../components/Button';
+import classNames from 'classnames';
+import {Button as signUpBtn} from '../../components/Button';
 
 
 class Signup extends Component {
@@ -25,19 +24,7 @@ class Signup extends Component {
     email: "",
     password: ""
 
-  };
-  // componentDidMount() {
-  //   this.loadSurvey();
-  // }
-
-  // loadSurvey = () => {
-  //   API.getSurvey()
-  //     .then(res =>
-  //       this.setState({})
-  //     )
-  //     .catch(err => console.log(err));
-  // };
-
+  }
 
   handleChange = event => {
     const { name, value } = event.target;
@@ -49,86 +36,57 @@ class Signup extends Component {
 
   handleFormSubmit = event => {
     event.preventDefault();
-    if (this.state.firstName && this.state.lastName && this.state.email && this.state.password) {
-      //write function inside of API for loadSurvey
-      API.createUser({
-        firstName: this.state.firstName,
-        lastName: this.state.lastName,
-        email: this.state.email,
-        password: this.state.password
-      })
-        // .then(res.redirect("/survey"))
-        .then(res => res.redirect('/'))
-        .catch(err => console.log(err));
-    }
+    // if (this.state.firstName && this.state.lastName && this.state.email && this.state.password) {
+    //   //write function inside of API for loadSurvey
+    //   API.({
+    //     firstName: this.state.firstName,
+    //     lastName: this.state.lastName,
+    //     email: this.state.email,
+    //     password: this.state.password
+    //   })
+    //     .then(res => this.loadSurvey())
+    //     .catch(err => console.log(err));
+    // }
   };
 
 
-  render() {
-    document.body.style.backgroundImage = `url("https://images.unsplash.com/photo-1485518882345-15568b007407?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9de002b94630e160d9e33c36decc06f3&auto=format&fit=crop&w=681&q=80")`
-    return (
-      <div className="signup-page">
-        <Header />
-        <div className="signup-form">
-          <Typography variant="headline" component="h3">
-            Were happy to have you here.
+render(){
+  document.body.style.backgroundImage = `url("https://images.unsplash.com/photo-1485518882345-15568b007407?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9de002b94630e160d9e33c36decc06f3&auto=format&fit=crop&w=681&q=80")`
+  return (
+  <div className= "signup-page">
+  <Header />
+  <div className="signup-form">
+        <Typography variant="headline" component="h3">
+          Were happy to have you here.
         </Typography>
-          <Typography component="p">
-            Please fill out the form below in order to continue to your personalized
-            profile.
+        <Typography component="p">
+          Please fill out the form below in order to continue to your personalized
+          profile.
         </Typography>
 
-          <TextField
+        <TextField
             id="firstName"
             label="firstName"
-            name="firstName"
+            name = "firstName"
             value={this.state.firstName}
             onChange={this.handleChange}
           />
 
-          <TextField
+        <TextField
             id="lastName"
             label="Last Name"
-            name="lastName"
+            name = "lastName"
             value={this.state.lastName}
             onChange={this.handleChange}
           />
 
           <TextField
-            id="email"
-            label="Email"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleChange}
+              id="email"
+              label="Email"
+              name= "email"
+              value={this.state.email}
+              onChange={this.handleChange}
           />
-
-
-          {/* <FormControl className={classNames()}> */}
-          <InputLabel htmlFor="adornment-password">Password</InputLabel>
-          <Input
-            id="adornment-password"
-            name="password"
-            type={this.state.showPassword ? 'text' : 'password'}
-            value={this.state.password}
-            onChange={this.handleChange}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="Toggle password visibility"
-                  onClick={this.handleClickShowPassword}
-                  onMouseDown={this.handleMouseDownPassword}
-                >
-                  {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-          {/* </FormControl> */}
-        </div>
-
-        {/* onclick of this button will create a new users account */}
-        <Button onClick={this.handleFormSubmit} children='Sign Up' />
-      </div>
 
           <FormControl className={classNames()}>
                <InputLabel htmlFor="adornment-password">Password</InputLabel>
@@ -161,8 +119,4 @@ class Signup extends Component {
 }}
 
 
-
-    );
-  }
-}
 export default Signup;
