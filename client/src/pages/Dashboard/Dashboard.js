9495c0b09ead270b5f782be1a18833f4cd253f13
Blonded <Lena@Lenas-MacBook-Pro.local>
2018-07-06 10:45:45 -0500

    working on dashboard

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,17 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import "./Dashboard.css";
 
-const Dashboard = () => (
-    
+class Dashboard extends Component {
+
+render(){
+    return(
     <div>
         <Navbar />
-        <h2>This is the Dashboard Page</h2>
-        <Footer />
+        <h2>Welcome!</h2>
+<div className = "row">
+<div className = "col-md-6">
+
+<div className="card">
+  <div className="card-body">
+<img className="card-img-top" src="http://i66.tinypic.com/vy842c.jpg" alt="Fashion">
+</img>
+  </div>
+</div>
+
+<div className="card">
+  <div className="card-body">
+  <img className="card-img-top" src="http://i64.tinypic.com/30seghy.jpg" alt="Card image cap">
+  </img>
+
+   <a href="#" class="btn btn-primary">Go somewhere</a>
+
+  </div>
+</div>
+
+<div className="card">
+  <div className="card-body">
+  <img className="card-img-top" src=".../100px180/" alt="Card image cap">
+  </img>
+
+  </div>
+</div>
+
+<div className="card">
+  <div className="card-body">
+  <img className="card-img-top" src=".../100px180/" alt="Card image cap">
+  </img>
+
+  </div>
+</div>
+
+
+</div>
+</div>
+// row1
+
+
+      <Footer />
     </div>
 
-);
+)}};
 
 
 export default Dashboard;
