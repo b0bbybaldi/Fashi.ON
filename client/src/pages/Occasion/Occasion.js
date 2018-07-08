9f39e9e0fa2ef6a8dc28647ec03980054c249f56
Mridula Udaya Shankar <mridu.sh92@gmail.com>
2018-07-08 15:43:34 -0700

    Add card to display suggestions on occasion page

--- client/src/pages/Occasion/Occasion.js
+++ client/src/pages/Occasion/Occasion.js
@@ -1,5 +1,4 @@
 import "./Occasion.css";
-export default Occasion;
 import React, { Component } from 'react';
 import Navbar from '../../components/Navbar/Navbar.js';
 import Footer from '../../components/Footer/Footer.js';
@@ -10,43 +9,50 @@ import Typography from '@material-ui/core/Typography';
 import OccasionCard from "../../components/OccasionCard";
 import API from '../../utils/API';
 
-class Dashboard extends Component {
+class Occasion extends Component {
 
-  state = {
-    suggestions: []
-  };
-  componentDidMount() {
-   this.asosAjaxCall();
-  }
-  
-  asosAjaxCall = () => {
-    API.getSuggestions()
-      .then(res => {
-        this.setState({ suggestions: res.data })
-        console.log(this.state.suggestions)
-      })
-      .catch(err => console.log(err));
-  }
+    state = {
+        suggestions: []
+    };
+    componentDidMount() {
+        this.asosAjaxCall();
+    }
+    asosAjaxCall = () => {
+        API.getSuggestions()
+            .then(res => {
+                this.setState({ suggestions: res.data })
+                console.log(this.state.suggestions)
+            })
+            .catch(err => console.log(err));
+    }
 
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <div className="paral paralsec3" id="paralsec3">
-          <div className="container-fluid">
-            <div className="row d-flex justify-content-center">
-              <div className="col-12 text-center">
-                <h3 className="display-3">Your Occasions</h3>
-              </div>
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <div class="col-12 col-sm-12 col-md-4">
+                    <div class="card">
+                        <a href="assets/img/homepage.jpg" class="fancybox">
+                            <img class="card-img-top" src="assets/img/homepage.jpg" alt="Card image cap" />
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title">Satiable Soiree</h5>
+                            <p class="card-text">Managing a food-gathering event can be easy now! Create your event, invite your friends and they decide which food to bring! Using modern tools like Node.js, Express, Handlebars and Sequelize, this group project was a great experience! </p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">HTML5, CSS3, Bootstrap</li>
+                            <li class="list-group-item">Javascript, jQuery, Moment.js, Typer.js</li>
+                            <li class="list-group-item">Node.js, Express, Handlebars, Sequelize, MySQL</li>
+                        </ul>
+                        <div class="card-body">
+                            <a href="https://github.com/Blonded/Satiable-Soiree" target="_blank" class="card-link">Repository</a>
+                            <a href="https://warm-dusk-10926.herokuapp.com/" target="_blank" class="card-link">Project Website</a>
+                        </div>
+                    </div>
+                </div>
+                <Footer />
             </div>
-            <div className="row d-flex justify-content-center">
-              <OccasionCard />
-            </div>
-          </div>
-        </div>
-        <Footer />
-      </div>
-    );
-  }
+        );
+    }
 }
-export default Dashboard;
+export default Occasion;
