62935a41c63d29c1abc5249bf577e20f144dc8fd
Blonded <Lena@Lenas-MacBook-Pro.local>
2018-07-09 16:17:31 -0700

    Occasions

--- client/src/components/HowToCard/HowToCard.js
+++ client/src/components/HowToCard/HowToCard.js
@@ -7,7 +7,7 @@ class HowToCard extends Component {
     return (
         <div className="col-12 col-sm-12 col-md-4">
             <div className="card">
-            <img className="card-img-top" src="https://images.unsplash.com/photo-1506485338023-6ce5f36692df?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=361443dfa9b671620076cd18e12d060d&auto=format&fit=crop&w=1650&q=80" alt="How to img" />
+            <img className="card-img-top responsive" src="https://images.unsplash.com/photo-1506485338023-6ce5f36692df?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=361443dfa9b671620076cd18e12d060d&auto=format&fit=crop&w=1650&q=80" alt="How to img" />
                 <div className="card-body">
                     <h3 className="card-title">
                     How to:
