ba8cc2d4463949fb4937e76d96967c8082f1f73b
Gustavo Gibo <gustavo.gibo@gmail.com>
2018-07-11 15:20:08 -0700

    Make suggestion controller

--- controllers/suggestionController.js
+++ controllers/suggestionController.js
@@ -7,21 +7,21 @@ module.exports = {
     getSuggestions: function (req, res) {
         console.log("reqqqqqqq");
         console.log("req.paramsssssss" ,req.params);
-        // console.log("req.params.dataaaaaa",req.params.data);
-        // const searchTerm = req.params.data;
-        // console.log(searchTerm)
-        // axios.get(`https://api.asos.com/product/search/v1/?q=${searchTerm}&store=2&lang=en-US&sizeschema=US&currency=USD&sort=freshness&channel=mobile-app&offset=0&limit=12`)
-        // .then(response=>{
-        //     console.log(response.data.products)
-        //     // response = JSON.parse(response);
-        //     res.json(response.data.products);
-        // })
-        // .catch(err=> console.log("err", err))
-        // function(err, response, body){
-        //     console.log("response", response);
-        //     console.log("body", body);
+        console.log("req.params.dataaaaaa",req.params.data);
+        const searchTerm = req.params.data;
+        console.log(searchTerm)
+        axios.get(`https://api.asos.com/product/search/v1/?q=${searchTerm}&store=2&lang=en-US&sizeschema=US&currency=USD&sort=freshness&channel=mobile-app&offset=0&limit=12`)
+        .then(response=>{
+            console.log(response.data.products)
+            // response = JSON.parse(response);
+            res.json(response.data.products);
+        })
+        .catch(err=> console.log("err", err))
+        function(err, response, body){
+            console.log("response", response);
+            console.log("body", body);
             
-        // })
+        })
     }
     
 };
